e6044c2c7565e5cd158875a411cf0cab79e9877b
araborg <arababs2015@gmail.com>
2026-05-23 17:02:15 +0100

    init nextjs: 15

--- the-wild-oasis-website-with-authjs/app/account/page.js
+++ the-wild-oasis-website-with-authjs/app/account/page.js
@@ -1,12 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
 	title: "Guest area",
 	// description: "Luxury Home From Home",
 };
 
-export default function page() {
+export default async function page() {
+	const session = await auth();
+	// console.log(session);
+
 	return (
 		<h2 className="font-semibold text-2xl text-accent-400 mb-7">
-			Welcome, Adeyemi
+			Welcome, {session.user.name}
 		</h2>
 	);
 }
