eb9ae84105074a27c0ee2e426a2b65ff177afb47
araborg <arababs2015@gmail.com>
2026-05-24 21:23:05 +0100

    init nextjs: 14

--- the-wild-oasis-website-with-authjs/app/_lib/actions.js
+++ the-wild-oasis-website-with-authjs/app/_lib/actions.js
@@ -13,10 +13,16 @@ export async function updateGuest(formData) {
 	if (!session) throw new Error("You must be logged in");
 
 	const nationalID = formData.get("nationalID");
-	const [nationality, countryFlag] = formData.get("nationality").split("%");
 
-	if (/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+	const [nationality, countryFlag] = formData.get("nationality").split("%");
+	console.log(nationality, countryFlag);
+
+	if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
 		throw new Error("Please provide a valid national ID");
+
+	const updateData = { nationality, countryFlag, nationalID };
+
+	console.log(updateData);
 }
 
 export async function signInAction() {
