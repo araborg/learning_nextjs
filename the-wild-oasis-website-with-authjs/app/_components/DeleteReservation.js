0301519a5a35580c31c9ecfcfe90435db7358d23
araborg <arababs2015@gmail.com>
2026-05-25 23:44:17 +0100

    init nextjs: 35

--- the-wild-oasis-website-with-authjs/app/_components/DeleteReservation.js
+++ the-wild-oasis-website-with-authjs/app/_components/DeleteReservation.js
@@ -4,6 +4,7 @@ import { deleteReservation } from "../_lib/actions";
 
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
 	// function deleteReservation() {
@@ -23,9 +24,17 @@ function DeleteReservation({ bookingId }) {
 			onClick={handleDelete}
 			className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
 		>
-			<TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+			{!isPending ? (
+				<>
+					<TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
 
-			<span className="mt-1">Delete</span>
+					<span className="mt-1">Delete</span>
+				</>
+			) : (
+				<span className="mt-1">
+					<SpinnerMini />
+				</span>
+			)}
 		</button>
 	);
 }
