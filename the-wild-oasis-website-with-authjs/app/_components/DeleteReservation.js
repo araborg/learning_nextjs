e8b6ea3f362e44c3017a77231b36a4fc9f3f4400
araborg <arababs2015@gmail.com>
2026-05-25 23:39:23 +0100

    init nextjs: 32

--- the-wild-oasis-website-with-authjs/app/_components/DeleteReservation.js
+++ the-wild-oasis-website-with-authjs/app/_components/DeleteReservation.js
@@ -12,9 +12,14 @@ function DeleteReservation({ bookingId }) {
 
 	const [isPending, startTransition] = useTransition();
 
+	function handleDelete() {
+		startTransition(() => deleteReservation(bookingId));
+	}
+
 	return (
 		<button
-			onClick={() => deleteReservation(bookingId)}
+			// onClick={() => deleteReservation(bookingId)}
+			onClick={handleDelete}
 			className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
 		>
 			<TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
