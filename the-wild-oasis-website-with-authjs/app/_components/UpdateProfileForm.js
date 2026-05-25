1f307c8fbe0ea5adf06ca66eb026a7abf1a1ab1c
araborg <arababs2015@gmail.com>
2026-05-25 16:12:23 +0100

    init nextjs: 20

--- the-wild-oasis-website-with-authjs/app/_components/UpdateProfileForm.js
+++ the-wild-oasis-website-with-authjs/app/_components/UpdateProfileForm.js
@@ -76,12 +76,14 @@ function UpdateProfileForm({ guest, children }) {
 }
 
 function Button() {
-	const x = useFormState();
-	console.log(x);
+	const { pending } = useFormState();
 
 	return (
-		<button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-			Update profile
+		<button
+			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+			disabled={pending}
+		>
+			{pending ? "Updating..." : "Update profile"}
 		</button>
 	);
 }
