26fc38a1b2cd65b9ee8befc23074b2ac6f0a46ba
araborg <arababs2015@gmail.com>
2026-05-26 20:47:51 +0100

    init nextjs: 75

--- the-wild-oasis-website-with-authjs/app/account/reservations/page.js
+++ the-wild-oasis-website-with-authjs/app/account/reservations/page.js
@@ -28,11 +28,7 @@ export default async function Page() {
 					</a>
 				</p>
 			) : (
-				<ul className="space-y-6">
-					{bookings.map((booking) => (
-						<ReservationCard booking={booking} key={booking.id} />
-					))}
-				</ul>
+				
 			)}
 		</div>
 	);
