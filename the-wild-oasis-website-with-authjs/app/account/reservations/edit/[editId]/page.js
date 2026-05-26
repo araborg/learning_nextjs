7047d71a25d84de3514509c21483a9930f06f45e
araborg <arababs2015@gmail.com>
2026-05-26 15:58:39 +0100

    init nextjs: 52

--- the-wild-oasis-website-with-authjs/app/account/reservations/edit/[editId]/page.js
+++ the-wild-oasis-website-with-authjs/app/account/reservations/edit/[editId]/page.js
@@ -1,15 +1,16 @@
-import { getBooking } from "@/app/_lib/data-service";
+import { getBooking, getCabin } from "@/app/_lib/data-service";
 
 export default async function Page({ params }) {
 	// console.log(params);
 
 	// CHANGE
-	const reservationId = 389;
-	const maxCapacity = 23;
+	// const reservationId = 389;
+	// const maxCapacity = 23;
 
 	// const cabin = await getCabin(cabinId);
-	const cabin = await getBooking(params.editId);
-	console.log(cabin);
+	const { id: reservationId, cabinId } = await getBooking(params.editId);
+	const { maxCapacity } = await getCabin(cabinId);
+	console.log(reservationId, maxCapacity);
 
 	return (
 		<div>
