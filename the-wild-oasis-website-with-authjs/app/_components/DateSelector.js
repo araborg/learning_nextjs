4b604e3117fab2318120b900f03206a1364c97c9
araborg <arababs2015@gmail.com>
2026-05-26 22:51:05 +0100

    init nextjs: 88

--- the-wild-oasis-website-with-authjs/app/_components/DateSelector.js
+++ the-wild-oasis-website-with-authjs/app/_components/DateSelector.js
@@ -1,6 +1,12 @@
 "use client";
 
-import { differenceInDays, isWithinInterval } from "date-fns";
+import {
+	differenceInDays,
+	isPast,
+	isSameDay,
+	isWithinInterval,
+} from "date-fns";
+
 import { DayPicker } from "react-day-picker";
 import { useReservation } from "./ReservationContext";
 
@@ -51,6 +57,10 @@ function DateSelector({ bookedDates, settings, cabin }) {
 				toYear={new Date().getFullYear() + 5}
 				captionLayout="dropdown"
 				numberOfMonths={2}
+				disabled={(curDate) =>
+					isPast(curDate) ||
+					bookedDates.some((date) => isSameDay(date, curDate))
+				}
 			/>
 
 			<div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
