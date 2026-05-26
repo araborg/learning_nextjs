714c664236b51b28cebd0ba96f774d8144dd556e
araborg <arababs2015@gmail.com>
2026-05-26 20:37:27 +0100

    init nextjs: 73

--- the-wild-oasis-website-with-authjs/app/_components/SubmitButton.js
+++ the-wild-oasis-website-with-authjs/app/_components/SubmitButton.js
@@ -2,7 +2,7 @@
 
 import { useFormStatus } from "react-dom";
 
-export default function SubmitButton() {
+export default function SubmitButton({ children, pendingLabel }) {
 	const { pending } = useFormStatus();
 
 	return (
@@ -10,7 +10,7 @@ export default function SubmitButton() {
 			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
 			disabled={pending}
 		>
-			{pending ? "Updating..." : "Update profile"}
+			{pending ? `${pendingLabel}...` : children}
 		</button>
 	);
 }
