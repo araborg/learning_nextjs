3e0d291583b67e4dd70e8702e8d5015558fc2503
araborg <arababs2015@gmail.com>
2026-05-26 22:41:46 +0100

    init nextjs: 86

--- the-wild-oasis-website-with-authjs/app/_components/DateSelector.js
+++ the-wild-oasis-website-with-authjs/app/_components/DateSelector.js
@@ -1,10 +1,10 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import { differenceInDays, isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
+import { useReservation } from "./ReservationContext";
 
 import "react-day-picker/dist/style.css";
-import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
 	return (
@@ -20,10 +20,13 @@ function DateSelector({ bookedDates, settings, cabin }) {
 	const { range, setRange, resetRange } = useReservation();
 
 	// CHANGE
-	const regularPrice = 23;
-	const discount = 23;
-	const numNights = 23;
+	// const regularPrice = 23;
+	// const discount = 23;
+	// const numNights = 23;
 	const cabinPrice = 23;
+
+	const { regularPrice, discount,  } = cabin;
+	const numNights; = differenceInDays(range)
 
 	// SETTINGS
 	// const minBookingLength = 1;
