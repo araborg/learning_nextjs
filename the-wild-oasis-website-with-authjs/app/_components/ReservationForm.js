7e91ebc7fd3776412a8e544588fc2c553f1b9075
araborg <arababs2015@gmail.com>
2026-05-27 09:31:37 +0100

    init nextjs: 112

--- the-wild-oasis-website-with-authjs/app/_components/ReservationForm.js
+++ the-wild-oasis-website-with-authjs/app/_components/ReservationForm.js
@@ -102,15 +102,13 @@ function ReservationForm({ cabin, user }) {
 							Start by selecting dates
 						</p>
 					) : (
-						{
-							/* <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-						Reserve now
-						</button> */
-						}(
-							<SubmitButton pendingLabel="Reserving...">
-								Reserve now
-							</SubmitButton>,
-						)
+						<SubmitButton pendingLabel="Reserving...">
+							Reserve now
+						</SubmitButton>
+
+						// <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+						// Reserve now
+						// </button>
 					)}
 				</div>
 			</form>
